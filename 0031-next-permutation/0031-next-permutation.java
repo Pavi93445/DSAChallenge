551886c326fc0222201a94939515a0680d1cf9c9
Pavithran K <pavithrank607@gmail.com>
2026-08-03 09:37:54 +0530

    Time: 0 ms (100%), Space: 44.1 MB (98.3%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,40 +1,44 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-      int point = -1;
-      for(int i = nums.length-1;i>0;i--){
-         if(nums[i-1] < nums[i]){
-            point = i-1;
-            break;
-         }
-      }
-      if(point == -1){
-        reverse(nums,0,nums.length-1);
-        return;
-      }
-      
-      int greatpoint = 0;
-      for(int j = nums.length-1; j>=0; j--){
-        if(nums[j] > nums[point]){
-            greatpoint = j;
-            break;
+        int point = -1;
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i - 1] < nums[i]) {
+                point = i - 1;
+                break;
+            }
         }
-      }
-      swap(nums,greatpoint,point);
-      reverse(nums,point+1,nums.length-1);
-      
+        if(point == -1){
+            reverse(nums,0,nums.length-1);
+            return;
+        }
+        int greater = 0;
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i] > nums[point]) {
+                greater = i;
+                break;
+            }
+        }
+        swap(nums,point,greater);  
+    reverse(nums,point+1,nums.length-1);
+
     }
-    public  void swap(int[] nums,int greatpoint,int point ){
-        int temp = nums[greatpoint];
-        nums[greatpoint] = nums[point];
-        nums[point] = temp;
-      }
-      public void reverse(int[] nums,int i,int j){
-         while(i < j){
-            int temp = nums[j];
-            nums[j] = nums[i];
-            nums[i] = temp;
-            i++;
-            j--;
-         }
-      }
+
+    
+
+    public void swap(int[] nums, int start, int end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
 }
